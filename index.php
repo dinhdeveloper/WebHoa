d88d7ab8774535712a3c4332a7022c18db3b906a
dinhdeveloper <dinhtrancntt@gmail.com>
2018-07-24 12:28:55 +0700

    ...........

--- index.php
+++ index.php
@@ -1,122 +1,123 @@
 <?php
-    session_start ();
-    include 'libs/database.php';
-	include("libs/GioHang.php");
-     //session_destroy();
-	if(isset($_SESSION["GioHang"]) == false){
-		$gioHang = new GioHang();
-		$_SESSION["GioHang"] = serialize($gioHang);
-	}
+session_start();
+include 'libs/database.php';
+include("libs/GioHang.php");
+//session_destroy();
+if (isset($_SESSION["GioHang"]) == false) {
+    $gioHang = new GioHang();
+    $_SESSION["GioHang"] = serialize($gioHang);
+}
 ?>
 <!doctype html>
 <html lang="vn">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Shop Flower Corner</title>
-	<link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
-		  integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style.css">
-	<script type="text/javascript" src="js/jquery/jquery-3.3.1.min.js"></script>
-	<script type="text/javascript" src="js/jquery/jquery-3.3.1.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shop Flower Corner</title>
+    <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+          integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="js/jquery/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery/jquery-3.3.1.js"></script>
     <script type="text/javascript" src="js/hieuungtuyetroi.js"></script>
 </head>
 <body style="overflow-x: hidden;">
 <?php include 'modules/mHeader.php'; ?>
 <div class="main-header">
-	<a href="index.php"><img src="images/icon/backroungh.jpg" class="img-backrounght"></a>
+    <a href="index.php"><img src="images/icon/backroungh.jpg" class="img-backrounght"></a>
 </div>
 <br>
 <div class="container">
-	<div class="row">
-		<div class="col-md-3">
+    <div class="row">
+        <div class="col-md-3">
             <?php
-                include 'modules/mMenu_left.php';
+            include 'modules/mMenu_left.php';
             ?>
-		</div><!-- menu-left-->
-		<div class="col-md-9">
+        </div><!-- menu-left-->
+        <div class="col-md-9">
             <?php
-                $c = 1;
-                if (isset($_GET['c'])) {
-                    $c = $_GET['c'];
-                }
-                switch ($c) {
-                    case 1:
-                        include 'pages/pDanhmucsanpham.php';
-                        break;
-                    case 2:
-                        include 'pages/pChitietsanpham.php';
-                        break;
-                    case 3:
-                        include 'pages/pXemthemsanpham.php';
-                        break;
-                    case 4:
-                        include 'modules/mHotrokhachhang.php';
-                        break;
-                    case 5:
-                        include 'giohang/quanlygiohang.php';
-                        break;
-                    case 6:
-                        include 'giohang/pthongtingiaohang.php'; // thông tin khách hàng
-                        break;
-                    case 7:
-                        include 'giohang/thongbaothanhcong.php';
-                        break;
-                        //khách hàng:
-                    case 10:
-                        include ("khachhang/pIndex.php"); //xử lý khách hàng
+            $c = 1;
+            if (isset($_GET['c'])) {
+                $c = $_GET['c'];
+            }
+            switch ($c) {
+                case 1:
+                    include 'pages/pDanhmucsanpham.php';
                     break;
-                    case 101:
-                        include 'giohang/themsanphamvaogiohang.php';
-                        break;
-                    case 102:
-                        include 'giohang/capnhatsoluongsanpham.php';
-                        break;
-                    case 103:
-                        include 'giohang/exThongTinGiaoHang.php'; // tạo thông tin
-                        break;
-					case 104:
-						include 'giohang/pdathang.php'; // đặt hàng
-						break;
-                    default:
-                        include("Error.php");
-                }
+                case 2:
+                    include 'pages/pChitietsanpham.php';
+                    break;
+                case 3:
+                    include 'pages/pXemthemsanpham.php';
+                    break;
+                case 4:
+                    include 'modules/mHotrokhachhang.php';
+                    break;
+                case 5:
+                    include 'giohang/quanlygiohang.php';
+                    break;
+                case 6:
+                    include 'giohang/pthongtingiaohang.php'; // thông tin khách hàng
+                    break;
+                case 7:
+                    include 'giohang/thongbaothanhcong.php';
+                    break;
+                //khách hàng:
+                case 10:
+                    include("khachhang/pIndex.php"); //xử lý khách hàng
+                    break;
+                case 101:
+                    include 'giohang/themsanphamvaogiohang.php';
+                    break;
+                case 102:
+                    include 'giohang/capnhatsoluongsanpham.php';
+                    break;
+                case 103:
+                    include 'giohang/exThongTinGiaoHang.php'; // tạo thông tin
+                    break;
+                case 104:
+                    include 'giohang/pdathang.php'; // đặt hàng
+                    break;
+                default:
+                    include("Error.php");
+            }
             ?>
-		</div><!-- danh sách sản phẩm-->
-	</div>
-	<div>
+        </div><!-- danh sách sản phẩm-->
+    </div>
+    <div>
         <?php
-            include 'pages/pSanphambannhieunhat.php';
-            include 'pages/pSanphamxemnhieunhat.php';
-            include 'modules/mHotrokhachhang.php';
+        include 'pages/pSanphambannhieunhat.php';
+        include 'pages/pSanphamxemnhieunhat.php';
+        include 'modules/mHotrokhachhang.php';
         ?>
-	</div>
+    </div>
 </div>
 <?php
 include 'modules/mFooter.php';
 ?>
 <!--nút cuộn lên đầu trang-->
 <script type="text/javascript">
-	(function () {
-		$('#cuon').click(function () {
-			$('body,html').animate({scrollTop: 0}, 600);
-			return false;
-		})
-	})(jQuery);
+    (function () {
+        $('#cuon').click(function () {
+            $('body,html').animate({scrollTop: 0}, 600);
+            return false;
+        })
+    })(jQuery);
 </script>
 <a href="#"><p><img src="images/icon/cuon.png" id="cuon"></p></a>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-		crossorigin="anonymous"></script>
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-		integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-		crossorigin="anonymous"></script>
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
-		integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
-		crossorigin="anonymous"></script>
+        integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+        crossorigin="anonymous"></script>
+
 </body>
 </html>
--- modules/mFooter.php
+++ modules/mFooter.php
@@ -1,11 +1,16 @@
 ﻿<div class="mini-icon" style=" position: fixed; bottom: 20px; left: 0;z-index: 999999 ">
-    <a href="https://www.messenger.com/t/canhdinhit" target="_blank" style="margin-left: 10px"><img src="images/icon/mess.png" alt=""></a>
-<!--    <a href="https://www.google.com/maps/place/212+Nguy%E1%BB%85n+%C4%90%C3%ACnh+Chi%E1%BB%83u,+Ph%C6%B0%E1%BB%9Dng+6,+Qu%E1%BA%ADn+3,+H%E1%BB%93+Ch%C3%AD+Minh,+Vietnam/@10.7787659,106.6878343,17z/data=!3m1!4b1!4m5!3m4!1s0x31752f3a9d92c4a7:0x6293396afcadc8a5!8m2!3d10.7787659!4d106.690023" target="_blank"><img src="images/icon/map.png" alt=""></a>-->
-<!--    <a href=""><img src="images/icon/map.png" alt=""></a>-->
-<!--    <a href="tel:"><img src="images/icon/sms.png" alt=""></a>-->
-<!--    <a href="#"><img src="images/icon/hotline.png" alt=""></a>-->
+    <div id="chatbox"></div>
+
+    <a href="https://www.messenger.com/t/canhdinhit" target="_blank" style="margin-left: 10px">
+        <img src="images/icon/mess.png" alt="">
+    </a>
+    <!--    <a href="https://www.google.com/maps/place/212+Nguy%E1%BB%85n+%C4%90%C3%ACnh+Chi%E1%BB%83u,+Ph%C6%B0%E1%BB%9Dng+6,+Qu%E1%BA%ADn+3,+H%E1%BB%93+Ch%C3%AD+Minh,+Vietnam/@10.7787659,106.6878343,17z/data=!3m1!4b1!4m5!3m4!1s0x31752f3a9d92c4a7:0x6293396afcadc8a5!8m2!3d10.7787659!4d106.690023" target="_blank"><img src="images/icon/map.png" alt=""></a>-->
+    <!--    <a href=""><img src="images/icon/map.png" alt=""></a>-->
+    <!--    <a href="tel:"><img src="images/icon/sms.png" alt=""></a>-->
+    <!--    <a href="#"><img src="images/icon/hotline.png" alt=""></a>-->
 </div>
-<br><div class="" style="background-color: #1C252C">
+<br>
+<div class="" style="background-color: #1C252C">
     <!-- Footer -->
     <footer class="page-footer font-small blue pt-4 mt-4" style="color: white">
         <!-- Footer Links -->
@@ -20,9 +25,9 @@
                         Shop hoa chúng tôi chuyên cung cấp các loại hoa tươi, hoa đẹp
                         được lấy từ nguồn gốc Đà Lạt về cùng với nhiều món hàng đặc biệt
                         dành cho quý khách.
-                        <p class="adam">
-                            Shop Flower Corner
-                        </p>
+                    <p class="adam">
+                        Shop Flower Corner
+                    </p>
                     </p>
                 </div>
                 <!-- Grid column -->
@@ -32,22 +37,28 @@
                     <!-- Links -->
                     <h5 class="text-uppercase">Liên hệ chúng tôi</h5><br>
                     <ul class="list-unstyled">
-                        <li >
-                            <a target="_blank" href="https://www.facebook.com/canhdinhit">
-								<img src="images/icon/fb.png" alt=""> Facebook</a>
-                        </li>
-                        <li>
-                            <a href="#"><img src="images/icon/tt.png" alt=""> Twitter</a>
-                        </li>
-                        <li>
-                            <a href="#"><img src="images/icon/g+.png" alt=""> Google Plus</a>
-                        </li>
-<!--                        <li>-->
-<!--                            <a href="#!"><img src="images/icon/dri.png" alt=""> Dribble</a>-->
-<!--                        </li>-->
-<!--                        <li>-->
-<!--                            <a href="#!"><img src="images/icon/pin.png" alt=""> Pinterest</a>-->
-<!--                        </li>-->
+                        <div>
+                            <li>
+                                <a target="_blank" href="https://www.facebook.com/canhdinhit">
+                                    <img src="images/icon/fb.png" alt=""> Facebook</a>
+                            </li>
+                        </div><br>
+                        <div>
+                            <li>
+                                <a href="#"><img src="images/icon/tt.png" alt=""> Twitter</a>
+                            </li>
+                        </div><br>
+                        <div>
+                            <li>
+                                <a href="#"><img src="images/icon/g+.png" alt=""> Google Plus</a>
+                            </li>
+                        </div>
+                        <!--                        <li>-->
+                        <!--                            <a href="#!"><img src="images/icon/dri.png" alt=""> Dribble</a>-->
+                        <!--                        </li>-->
+                        <!--                        <li>-->
+                        <!--                            <a href="#!"><img src="images/icon/pin.png" alt=""> Pinterest</a>-->
+                        <!--                        </li>-->
                     </ul>
                 </div>
                 <!-- Grid column -->
@@ -57,11 +68,14 @@
                     <h5 class="text-uppercase">Thông tin liên lạc</h5><br>
                     <div>
                         <img src="images/icon/home.png" alt="">
-                        <a href="https://www.google.com/maps/place/212+Nguy%E1%BB%85n+%C4%90%C3%ACnh+Chi%E1%BB%83u,+Ph%C6%B0%E1%BB%9Dng+6,+Qu%E1%BA%ADn+3,+H%E1%BB%93+Ch%C3%AD+Minh,+Vietnam/@10.7787659,106.6878343,17z/data=!3m1!4b1!4m5!3m4!1s0x31752f3a9d92c4a7:0x6293396afcadc8a5!8m2!3d10.7787659!4d106.690023" target="_blank">212 Nguyễn Đình Chiểu, Q.3, TP.HCM</a>
-                    </div><br>
+                        <a href="https://www.google.com/maps/place/212+Nguy%E1%BB%85n+%C4%90%C3%ACnh+Chi%E1%BB%83u,+Ph%C6%B0%E1%BB%9Dng+6,+Qu%E1%BA%ADn+3,+H%E1%BB%93+Ch%C3%AD+Minh,+Vietnam/@10.7787659,106.6878343,17z/data=!3m1!4b1!4m5!3m4!1s0x31752f3a9d92c4a7:0x6293396afcadc8a5!8m2!3d10.7787659!4d106.690023"
+                           target="_blank">212 Nguyễn Đình Chiểu, Q.3, TP.HCM</a>
+                    </div>
+                    <br>
                     <div>
                         <img src="images/icon/email.png" alt=""> <a href="mailto:Dinhtrancntt@gmail.com">Dinhtrancntt@gmail.com</a>
-                    </div><br>
+                    </div>
+                    <br>
                     <div>
                         <img src="images/icon/phone.png" alt="">
                         <a href="tel:0975469232">
